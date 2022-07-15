12879637aee180454c639bec379a14c016faeca6
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-07-15 13:36:32 -0700

    Test 6 PastryCost Green

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -4,11 +4,17 @@ namespace Bakery
 {
   public class Pastry
   {
+    private static int _Cost = 2;
     public int Amount { get; set; }
 
     public Pastry(int amount)
     {
       Amount = amount;
+    }
+
+    public static int Cost
+    {
+      get { return _Cost; }
     }
 
     public bool IsPastry()
