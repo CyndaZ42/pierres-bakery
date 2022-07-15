8c33bf6a6230d879ce46e2d60bf46570ac1bf9ff
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-07-15 14:42:35 -0700

    program wip

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,11 +1,20 @@
 using System;
+using System.Threading;
 
 namespace Bakery {
   public class Program
   {
     public static void Main()
     {
-
+      Console.WriteLine("#───────────────────#");
+      Console.WriteLine("|  Pierre's Bakery  |");
+      Console.WriteLine("#───────────────────#");
+      char[] welcome = { 'W','e','l','c','o','m','e',' ','t','o',' ','P','i','e','r','r','e','s',' ','B','a','k','e','r','y','!'};
+      foreach (char letter in welcome)
+      {
+        Console.Write(letter);
+        Thread.Sleep(70);
+      }
     }
   }
 }
